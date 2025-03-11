7e7e5ac716774c5bb015f4383e0ae707fd83692d
Arjun Maniyani <arjunrmaniyani@gmail.com>
2025-03-11 12:25:53 +0530

    Changed hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(void) {
+  cout << "SRN: PES2UG22CS095";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
